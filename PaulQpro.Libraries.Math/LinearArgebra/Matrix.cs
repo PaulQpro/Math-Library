a4983aa9b9107f78c09051c6f88c397efc1af435
PaulQpro <paulqpro@mail.ru>
2022-11-24 22:10:45 +0300

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit 223cfc9456ee1977d30e4240fb712b96992cbeb6.

---
+++ PaulQpro.Libraries.Math/LinearArgebra/Matrix.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaulQpro.Math
+{
+    static public partial class Linear 
+    { 
+        public class BaseMatrix
+        {
+            public virtual int[] Dimensions { get; }
+        }
+    }
+}
