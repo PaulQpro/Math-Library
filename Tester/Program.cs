05c66e6f9fbccf0e91f9d61cd3f17c639616d4d9
PaulQpro <paulqpro@mail.ru>
2022-11-24 22:08:30 +0300

    Добавьте файлы проекта.

---
+++ Tester/Program.cs
@@ -0,0 +1,4 @@
+﻿using PaulQpro.Math;
+Linear.Vector vector = new(new double[] { 0, 1, 2, 3, 5 });
+Console.WriteLine("Vector vector = {0} is {1} dimensional",vector.ToString(),vector.Dimensions);
+Console.ReadKey();
